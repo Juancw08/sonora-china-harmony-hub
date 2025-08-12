70c1b8d13a042892c0c4d3d4b8118da5af94316c
Juan C <juancw08@outlook.com>
2025-08-11 22:52:45 -0700

    updated base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/sonora-china-harmony-hub/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
